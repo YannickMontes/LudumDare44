3f5b1825299d5f878fa7bbbe363b7797b6218201
NicolasDuboc <nicolas-duboc@hotmail.fr>
2019-04-28 20:08:18 +0200

    animation

---
+++ Unity/Assets/Scripts/Shaders/Waves_Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves_Settings : MonoBehaviour
+{
+
+    float pannerSpeed = Random.Range(0.0f, 1.0f);
+    float vertexOffset = Random.Range(0.0f, 1.0f);
+
+    Renderer meshRenderer;
+    Material instancedMaterial; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        meshRenderer = gameObject.GetComponent<Renderer>();
+        instancedMaterial = meshRenderer.material; 
+        //instancedMaterial.SetFloat ("_OffsetValue")
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
